6d89ab6e0da66957b98fb343a38a474293a9eb15
Jonathan <jmejias986@protonmail.com>
2020-11-17 19:50:46 -0600

    Configuraciones

--- PaginaBlog/settings.py
+++ PaginaBlog/settings.py
@@ -149,9 +149,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = {
-    os.path.join(BASE_DIR, 'static'),
-}
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 
 # CONFIGURACION PARA ENVIAR CORREOS
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
