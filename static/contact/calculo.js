81eb85030d8772bbbc0ecb8a9f9c4ccaedad144e
Jonathan <jmejias986@protonmail.com>
2020-11-19 17:43:58 -0600

    nuevo archivo js

--- static/contact/calculo.js
+++ static/contact/calculo.js
@@ -4,8 +4,8 @@ for (var i = 0; i < 35; i++){
 }
 
 function fib(n){
-    if (n == 0 || n==1) return 1
-    if (memo[n] > 0) return memo[n]
+    if (n == 0 || n==1){return 1}
+    if (memo[n] > 0) {return memo[n]}
     memo[n] = memo[n-1] + memo[n-2]
     return memo[n]
 }
@@ -13,7 +13,8 @@ function fib(n){
 function getFib(){
     fecha = document.getElementById("fecha").value;
     day = fecha[8]+fecha[9]
-    alert(day + ', fib: ', fib(Number(day)))
+    if (fecha[8] == '0'){day = fecha[8]+fecha[9]}
+    alert(day + ', fib: ' + fib(Number(day)))
 }
 
 document.getElementById('btn1').addEventListener('click', getFib);
