33f53c2a66f9167cef761f1e709149d6000187ce
Jonathan <jmejias986@protonmail.com>
2020-11-19 19:46:48 -0600

    Envio de correo

--- PaginaBlogApp/views.py
+++ PaginaBlogApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-from .form import Formulario_Contacto
+from form import Formulario_Contacto
+from PaginaBlog import settings 
 
 # Create your views here.
 
@@ -9,7 +10,7 @@ def home(request):
 
 def about_us(request):
     return render(request, 'PaginaBlogApp/aboutus.html')
-
+"""
 def contact(request):
     if request.method == "POST":
         data = {
@@ -34,3 +35,21 @@ def contact(request):
         f = Formulario_Contacto()
     
     return render(request, "PaginaBlogApp/contact.html")
+"""
+
+def contact(request):
+    if request.method == "POST":
+        data = {
+            'nombre': request.POST.get('name'),
+            'apellidos': request.POST.get('apellido'),
+            'mail': request.POST.get('mail')
+        }
+        mensaje = 'El cliente ' + data['nombre'] + ' desea recibir mas información, su correo es: ' + request.POST.get('mail')
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ['lucasvaan80@gmail.com']
+
+        send_mail('subject', mensaje, email_from, recipient_list)
+    
+    return render(request, "PaginaBlogApp/contact.html")
+
+
