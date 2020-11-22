005e700a17798d03ea36c23f63eed3b462776780
Jonathan <jmejias986@protonmail.com>
2020-11-22 13:57:37 -0600

    Envio de correo

--- static/contact/correo.js
+++ static/contact/correo.js
@@ -1,3 +1,25 @@
+function fib(n){
+	var a = 0
+	var b = 1
+	for(var i = 2; i<Number(n+1); i++){
+		var new_a = 0 * a + 1 * b
+		var new_b = 1 * a + 1 * b
+		
+		a = new_a
+        b = new_b
+    }
+	return b
+}
+
+function getFib(){
+    fecha = document.getElementById("fecha").value;
+    day = fecha[8]+fecha[9]
+    if (fecha[8] == '0'){day = fecha[9]}
+    alert('Todo correcto, nos pondremos en contacto pronto\n' + 
+    '**Sabias que ' + fib(Number(day)) + ', es el fibonacci de ' + day 
+    + ', el dia de tu cumpleaños :)**')
+}
+
 const btn = document.getElementById('btn1');
 
 document.getElementById('formulario')
@@ -12,9 +34,9 @@ document.getElementById('formulario')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Enviado';
-      alert('Correo enviado!');
+      getFib()
     }, (err) => {
-      btn.value = 'Enviado';
+      btn.value = 'Enviado, aunque hubo un error';
       alert(JSON.stringify(err));
     });
 });
