c767b9c1beb1363dc5869289db11aecdd26f4fdb
Jonathan <jmejias986@protonmail.com>
2020-11-23 14:58:43 -0600

    Reddit API

--- static/base/base.js
+++ static/base/base.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
         $("#search").toggle();        
         $("#btnSearch").toggle();        
     });
+
+
+    window.addEventListener('load', () => { 
+        const button = document.querySelector('#btnSearch'); 
+        button.addEventListener('click', () => { 
+            document.querySelector('#search').value = ""; 
+        }); 
+    }); 
+
 });
