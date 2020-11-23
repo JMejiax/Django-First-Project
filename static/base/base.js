c7144966d14a9b94837be81ab231ac1e79bab8d3
Jonathan <jmejias986@protonmail.com>
2020-11-23 13:57:32 -0600

    Reddit API

--- static/base/base.js
+++ static/base/base.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+    $("#btnSearch").click(function () {
+        $("#search").fadeOut(3000);
 
+    });
+    // $("#btnSearch").click(function () {
+    //     $("#search").slideDown(5000);
+
+    // });
+
+});
