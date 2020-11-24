8e6a36a6b0015e10eef5850d5a3c29856613f3cf
Jonathan <jmejias986@protonmail.com>
2020-11-23 22:19:23 -0600

    Reddit API

--- static/base/base.js
+++ static/base/base.js
@@ -1,26 +1,33 @@
 $(document).ready(function () {
 
-    $("#search").hide();        
-    $("#btnSearch").hide(); 
-    $("#buttonIn").hide(); 
+    $("#search").hide();
+    $("#btnSearch").hide();
+    $("#buttonIn").hide();
 
     $("#mostrar").click(function () {
-        $("#search").toggle();        
-        $("#btnSearch").toggle();   
+        $("#search").toggle();
+        $("#btnSearch").toggle();
         $("#mostrar").hide();
     });
 
+    $("#btnSearch").click(function () {
+        if (("#search").value == '') {
+            $("#search").toggle();
+            $("#btnSearch").toggle();
+            $("#mostrar").show();
+        }
+    });
     // $("#navbar1").click(function () {
     //     $("#search").hide();        
     //     $("#btnSearch").hide(); 
     //     $("#mostar").show();
     // });    
 
-    window.addEventListener('load', () => { 
-        const button = document.querySelector('#btnSearch'); 
-        button.addEventListener('click', () => { 
-            document.querySelector('#search').value = ""; 
-        }); 
-    }); 
+    window.addEventListener('load', () => {
+        const button = document.querySelector('#btnSearch');
+        button.addEventListener('click', () => {
+            document.querySelector('#search').value = "";
+        });
+    });
 
 });
