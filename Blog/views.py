4d307362bc32e43ab677bd5e498ec8b78c8533d3
Jonathan <jmejias986@protonmail.com>
2020-11-24 12:50:46 -0600

    Reddit API

--- Blog/views.py
+++ Blog/views.py
@@ -19,14 +19,17 @@ def reddit(request):
     if request.method == 'POST':
         art = []
         search = request.POST.get("search", "")
-        to_search = search.replace(' ', '')
-        reddit = praw.Reddit(client_id="q1i340R54PKFwg", client_secret="moaYGZScmCG_t131e5zGOVoW8FkHDg", user_agent="jm_cys")
-        try:
-            for submission in reddit.subreddit(to_search).hot(limit=10):
-                art.append((submission.title, submission.author, submission.score, submission.url))
-        except Exception:
+        if search != '':
+            to_search = search.replace(' ', '')
+            reddit = praw.Reddit(client_id="q1i340R54PKFwg", client_secret="moaYGZScmCG_t131e5zGOVoW8FkHDg", user_agent="jm_cys")
+            try:
+                for submission in reddit.subreddit(to_search).hot(limit=10):
+                    art.append((submission.title, submission.author, submission.score, submission.url))
+            except Exception:
+                pass
+            data = {"articulos": art, "busqueda": search}
+            return render(request, 'Blog/reddit.html', data)
+        else:
             pass
-        data = {"articulos": art, "busqueda": search}
-        return render(request, 'Blog/reddit.html', data)
     else:
         return render(request, 'Blog/reddit.html')
