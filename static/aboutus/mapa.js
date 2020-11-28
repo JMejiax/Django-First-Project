e9948ec50211225fcc1b67d71b0684c2726722e1
Jonathan <jmejias986@protonmail.com>
2020-11-28 17:51:39 -0600

    Mejoras al map

--- static/aboutus/mapa.js
+++ static/aboutus/mapa.js
@@ -1,31 +1,18 @@
-// Muestre solo el mapa
-function iniciarMap2(){
-    var coord = {lat:10.106365 ,lng:-84.252139};
-
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 14,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(iniciarMap);
+  }
 }
 
-function iniciarMap(){
+
+function iniciarMap(position){
   var dd = new google.maps.DirectionsRenderer();
   var ds = new google.maps.DirectionsService();
 
 
   var guatusa = new google.maps.LatLng(10.106365, -84.252139);
-  if (navigator.geolocation) {
-    var somewhere = navigator.geolocation.getCurrentPosition(function pos(position){
-      var place = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      return place;
-    });
-  } else {
-    var somewhere = new google.maps.LatLng(10.423129, -83.755066);
-  }
+  var somewhere = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  // var somewhere = new google.maps.LatLng(10.423129, -83.755066);
 
   var options = {
     zoom: 14,
