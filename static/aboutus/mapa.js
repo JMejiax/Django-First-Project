1a27c1ef09eaea7f7c3f31759a9e94e6db48702f
Jonathan <jmejias986@protonmail.com>
2020-11-28 17:58:17 -0600

    Mejoras al map

--- static/aboutus/mapa.js
+++ static/aboutus/mapa.js
@@ -12,6 +12,7 @@ function iniciarMap(position){
 
   var guatusa = new google.maps.LatLng(10.106365, -84.252139);
   var somewhere = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  console.log(position.coords.latitude, position.coords.longitude)
   // var somewhere = new google.maps.LatLng(10.423129, -83.755066);
 
   var options = {
